9e817bbace61cd213cda2d7b25252e506316f2a4
Arjun Kumar <Arjun@s-28-135.flex.volo.net>
2018-10-21 02:15:00 -0500

    Completed Brakes

---
+++ CentralComputing/Brake.cpp
@@ -0,0 +1,31 @@
+#include "Brake.hpp"
+using namespace Utils;
+
+
+Brake::Brake(){
+
+  
+
+}
+
+void Brake::enable_Brakes() {
+ 
+}
+
+void Brake::disable_Brakes() {
+  
+}
+
+void Brake::set_enable(bool enable){
+
+ 
+}
+
+int16_t Brake::calculate_throttle(double dt, int16_t last_throttle){
+  
+
+  }
+void Brake::set_throttle(int16_t value){
+
+  }
+
---
+++ CentralComputing/Brake.hpp
@@ -0,0 +1,80 @@
+#ifndef BRAKE_HPP
+#define BRAKE_HPP
+
+#include "Utils.h"
+#include "SourceManager.hpp"
+#include "MotionModel.hpp"
+#include <cmath>
+
+//TODO: Better define these constants
+#define RPM_PDR 600.0  //point of diminishing returns
+#define DISK_CIRCUMFRENCE 10.0 //In Meters
+#define RPM_TO_MS (DISK_CIRCUMFRENCE/60.0) // RPM to Meters per second
+#define MS_TO_RPM (60.0/DISK_CIRCUMFRENCE) // Meters per second to RPM
+//TODO: Better define this constant
+#define RPM_TO_USEC (25.0/400.0) // Usec/RRPM
+
+#define Brake_OFF -50.0
+
+
+#define KP 1.0
+#define KD 0.0
+#define KI 1.0
+
+class Brake {
+  public:
+
+    /*
+     * Constructor
+     * Setup the PWM pins
+     */
+    Brake();
+
+    /*
+     * Arms the Brakes
+     */
+    void enable_Brakes();
+
+    /*
+     * Disarms the Brakes
+     */
+    void disable_Brakes();
+
+    /*
+     * is_enabled returns enabled variable
+     */
+
+
+
+    
+
+    bool is_enabled();
+
+    /*
+     * Uses a PI loop to calculate the appropriate Brake throttle
+     */
+    int16_t calculate_brakepower(double dt, int16_t last_throttle);
+
+    /*
+     * Sets the Brake throttle to a specific value
+     */
+    void set_brakepower(int16_t value);
+
+    void debug_set_pwm_pin(std::string & path);
+
+  private:
+    int16_t Brake::calculate_throttle(double dt, int16_t last_throttle);
+    void Brake::set_throttle(int16_t value);
+
+    void set_enable(bool enable);
+
+    static std::string pwm_chip;
+    static std::string pwm_pin;
+
+    double integral;
+
+    bool enabled;
+
+};
+
+#endif
