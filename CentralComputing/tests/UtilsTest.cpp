808c0949d2de658834ca0069ce0fb906674e5141
wtbutler <wtbutler2@gmail.com>
2018-10-25 20:00:39 -0500

    Added thread tests
    Tests single thread and multi thread. I think it locked it
    to a single core automatically.

--- CentralComputing/tests/UtilsTest.cpp
+++ CentralComputing/tests/UtilsTest.cpp
@@ -25,3 +25,44 @@ TEST(UtilsTest, busyWaitTestLong) {
 	Utils::busyWait(time);
 	EXPECT_GE(std::clock(), start + time);
 }
+
+TEST(UtilsTest, busyWaitThreadTest) {
+	double start = std::clock();
+	int time = 100000;
+	struct timespec currTime;
+
+	std::thread t([&](){ Utils::busyWait(time); ;
+	clockid_t threadClockId;
+	pthread_getcpuclockid(pthread_self(), &threadClockId);
+	clock_gettime(threadClockId, &currTime); });
+
+	
+	EXPECT_GE(currTime.tv_sec * 1000000 + currTime.tv_nsec / 1000, start + time);
+	t.join();
+}
+
+TEST(UtilsTest, busyWaitManyThreadTest) {
+	double start = std::clock();
+	int time = 100000;
+	struct timespec thread1;
+	struct timespec thread2;
+
+	std::thread t1([&](){ 
+		Utils::busyWait(time); ;
+		clockid_t threadClockId;
+		pthread_getcpuclockid(pthread_self(), &threadClockId);
+		clock_gettime(threadClockId, &thread1); 
+	});
+
+	std::thread t2([&](){
+		Utils::busyWait(time); ;
+		clockid_t threadClockId;
+		pthread_getcpuclockid(pthread_self(), &threadClockId);
+		clock_gettime(threadClockId, &thread2); 
+	});
+
+	
+	EXPECT_GE((thread1.tv_sec + thread2.tv_sec) * 1000000 + (thread1.tv_nsec + thread2.tv_nsec) / 1000, start + time * 2);
+	t1.join();
+	t2.join();
+}
